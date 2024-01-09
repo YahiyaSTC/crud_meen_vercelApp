e362a2ae4bbcbaaf7e72f948a6eeb37067eaf783
YahiyaSTC <106611481+YahiyaSTC@users.noreply.github.com>
2024-01-09 14:30:43 +0500

    Add files via upload

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  let uri = "mongodb+srv://yahiyastc:HhtbKyyQ7xbufZhH@cluster0.9mqjtwm.mongodb.net/crudOperationsApp"
   try {
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+      // const conn = await mongoose.connect(process.env.MONGO_URI);
+      const conn = await mongoose.connect(uri);
       console.log(`Database Has Been Connected ${conn.connection.host}/${conn.connection.name}`)
     //   console.log(process.mainModule.path);
   } catch (error) {
